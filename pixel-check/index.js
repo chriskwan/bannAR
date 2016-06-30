86b7610b0a3d846530379a493b75fe3de933d703
Chris Kwan <chriswkwan@gmail.com>
2016-06-30 18:05:03 -0400

    Add clicking colors

--- pixel-check/index.js
+++ pixel-check/index.js
@@ -1,5 +1,6 @@
 var useVideo = true;
 var colorThreshold = 50;
+var targetColor;
 
 var colorDistance = function(color1, color2) {
   var rSquare = Math.pow((color1.r - color2.r), 2);
@@ -61,15 +62,49 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // }
 
   //red
-  var targetColor = {
+  targetColor = {
     r: 211,
     g: 73,
     b: 62
   };
 
-  var context = document.getElementById('myvid').getContext('2d')
+  var myVid = document.getElementById('myvid');
+  var context = myVid.getContext('2d')
   var draw = function(frame, dt) {
     update(context, frame, targetColor)
   }
   var myCamvas = new camvas(context, draw);
-})
+
+  myVid.addEventListener("click", function(event) {
+    return getColor(event, myVid, context);
+  });
+});
+
+function getColor(event, video, context) {
+  var x = event.offsetX;
+  var y = event.offsetY;
+
+  var width = video.width;
+  var pixelIndex = (y * width + x) * 4;
+
+  //console.log("You clicked on: " + x + ", " + y + " pixel should be " + pixelIndex);
+
+  var imageData = context.getImageData(0, 0, video.width, video.height);
+  var pixels = imageData.data;
+
+  // Find the color of the pixel you clicked on
+  for (var i=0; i<pixels.length; i += 4) {
+    if (i === pixelIndex) {
+      var r = pixels[i];
+      var g = pixels[i+1];
+      var b = pixels[i+2];
+
+      //console.log("Color is: " + r + ", " + g + ", " + b);
+      targetColor = {
+        r: r,
+        g: g,
+        b: b
+      }
+    }
+  }
+}
