249edf992344cd878d67a313e5d37085578f6fd0
Chris Kwan <chriswkwan@gmail.com>
2016-06-30 17:05:55 -0400

    Add code to gh-pages

---
+++ pixel-check/index.js
@@ -0,0 +1,75 @@
+var useVideo = true;
+var colorThreshold = 50;
+
+var colorDistance = function(color1, color2) {
+  var rSquare = Math.pow((color1.r - color2.r), 2);
+  var gSquare = Math.pow((color1.g - color2.g), 2);
+  var bSquare = Math.pow((color1.b - color2.b), 2);
+  var dist = Math.sqrt(rSquare + gSquare + bSquare);
+  return dist;
+}
+
+var update = function(context, frame, targetColor) {
+  var width = context.canvas.width;
+  var height = context.canvas.height;
+
+  if (useVideo) {
+    context.drawImage(frame, 0, 0);
+  } else {
+    // using sample image
+    image = document.getElementById("sceneImage");
+    context.drawImage(image, 0, 0, width, height);
+  }
+
+  var pixels = context.getImageData(0, 0, width, height).data;
+  var newPixels = pixels;
+
+  for (var pixelX = 0; pixelX < width; pixelX++) {
+    for (var pixelY = 0; pixelY < height; pixelY++) {
+      var pixelIndex = (pixelY * width + pixelX) * 4;
+      var rIndex = pixelIndex;
+      var gIndex = pixelIndex + 1;
+      var bIndex = pixelIndex + 2;
+      var alphaIndex = pixelIndex + 3;
+
+      if (colorDistance(targetColor, {
+        r: pixels[rIndex],
+        g: pixels[gIndex],
+        b: pixels[bIndex]
+      }) < colorThreshold) {
+        newPixels[rIndex] = 0;
+        newPixels[gIndex] = 255;
+        newPixels[bIndex] = 0;
+        newPixels[alphaIndex] = 125;
+      }
+    }
+  }
+
+  var newImageData = context.createImageData(width, height);
+  for (var i=0; i<pixels.length; i++) {
+    newImageData.data[i] = newPixels[i];
+  }
+  context.putImageData(newImageData, 0, 0);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  //cwkTODO post-it yellow-ish
+  // var targetColor = {
+  //   r: 140,
+  //   g: 136,
+  //   b: 88
+  // }
+
+  //red
+  var targetColor = {
+    r: 211,
+    g: 73,
+    b: 62
+  };
+
+  var context = document.getElementById('myvid').getContext('2d')
+  var draw = function(frame, dt) {
+    update(context, frame, targetColor)
+  }
+  var myCamvas = new camvas(context, draw);
+})
